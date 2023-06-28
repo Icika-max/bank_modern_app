3a31fe94268da40f9e43d3de67e4f93e6cf87d0c
icika-max <icikakennedy@gmail.com>
2023-06-28 16:08:38 +0300

    assets and tailwind configuration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <h1>Hellow world</h1>
+
+    </div>
+  )
+}
+
+export default App
